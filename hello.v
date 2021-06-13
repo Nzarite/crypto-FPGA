1fce14d7b30c955d3a0b017ab0a2f140b58d053b
Nzarite <84867574+Nzarite@users.noreply.github.com>
2021-06-14 00:20:56 +0530

    Delete hello.v

--- hello.v
+++
@@ -1,9 +0,0 @@
-module hello();
-
-initial
-  begin
-    $display("Hello World!");   // This will display a message
-    $finish ; // This causes the simulation to end.  Without, it would go on..and on.
-  end
-
-endmodule
